cc7c3e667091e346b88de4a04cffc00002f15b46
Kha Bui <khadbui@gmail.com>
2018-04-15 00:26:46 -0700

    Fix avr.c
    _Fix Port
    _Complete external crystal clock = 8Mhz

--- Project1/Project1/avr.c
+++ Project1/Project1/avr.c
@@ -1,54 +1,63 @@
 #include <avr/io.h>
 #include "avr.h"
 
-
 #define WAITTIME 500
 void ini_avr(void)
 {
-  //Define PORTA and input
-  PORTA = 0x00;
-  DDRA = 0x00;
+	//Define PORTB_1 and input
+	PORTB = 0x01;
+	DDRB = 0x00;
 
-  //Define PORTB and output
-  PORTB = 0x00;
-  DDRB = 0x01;
+	//Define PORTB_0 and output
+	PORTB = 0x00;
+	DDRB = 0x01;
 
 }
 
 void wait_avr(unsigned short msec)
 {
-  TCCR0 = 3;
-  while (msec--) {
-    TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
-    SET_BIT(TIFR, TOV0);
-    while (!GET_BIT(TIFR, TOV0));
-  }
-  TCCR0 = 0;
+	TCCR0 = 3;
+	while (msec--) {
+		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
+		SET_BIT(TIFR, TOV0);
+		while (!GET_BIT(TIFR, TOV0));
+	}
+	TCCR0 = 0;
 }
 
 int main (void)
 {
-  //init
-  ini_avr();
+	/*init*/
+	ini_avr();
 
-	
-  while(1){
-	  if (!GET_BIT(PINB,1)){
-		  volatile long i = 0;
-		  SET_BIT(PORTB, 0);
-		  while (i < 14000){
-			  ++i;
-		  }
-		  i =0;
-		  CLR_BIT(PORTB, 0);
-		  while (i < 14000){
-			  ++i; 
-		  }
-		  
-	  }
-	  else {
-		  CLR_BIT(PORTB, 0);
-	  }
-	  
-  }
+	while(1){
+		//BLinking LED Without external crystal
+		/*if (!GET_BIT(PINB,1)){
+		volatile long i = 0;
+		SET_BIT(PORTB, 0);
+		while (i < 14000){
+		++i;
+		}
+		i =0;
+		CLR_BIT(PORTB, 0);
+		while (i < 14000){
+		++i;
+		}
+		
+		}
+		else {
+		CLR_BIT(PORTB, 0);
+		}*/
+		
+		//Blinking LED with EXTERNAL CRYSTAL
+		//Using: Medium Freq: Start-up time: 16 CK + 64ms
+		if (!GET_BIT(PINB, 1)){
+			SET_BIT(PORTB, 0);
+			wait_avr(WAITTIME);
+			CLR_BIT(PORTB, 0);
+			wait_avr(WAITTIME);
+			}else{
+			CLR_BIT(PORTB, 0);
+		}
+	}
 }
