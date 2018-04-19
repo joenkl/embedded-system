f397cb70c046a4c9a2f6f956121ed6f756a4da89
Kha Bui <khadbui@gmail.com>
2018-04-19 16:33:06 -0700

    Fixed is_pressed()-WORK

--- Project2/p2_LCD/main.c
+++ Project2/p2_LCD/main.c
@@ -33,18 +33,15 @@ unsigned char numToChar(int num){
 //Need to fix
 unsigned int is_pressed(int r, int c){
 	c += 4;
-	/*//Set row to output and STRONG 0
+	//Set row to output and STRONG 0
 	SET_BIT(DDRC, r);
 	CLR_BIT(PORTC, r);
 	
 	//Set column to input and WEAK 1
 	CLR_BIT(DDRC, c);
-	SET_BIT(PORTC, c);*/
+	SET_BIT(PORTC, c);
 	
-	if (!GET_BIT(PINC, c))
-		return 1;
-	else
-		return 0;
+	return (GET_BIT(PINC, c)) == 0 ? 1 : 0;
 }
 
 unsigned int get_key(){
@@ -61,36 +58,24 @@ unsigned int get_key(){
 
 int main(void)
 {
-	DDRC = 0x0F;
-	PORTC = 0xF0;
 	DDRB = 0x08; //set B3 output
 	ini_lcd();
 	/* Replace with your application code */
 	while (1)
 	{
 		//Set to default
-		//DDRC = 0x00;
-		//PORTC = 0x00;
+		DDRC = 0x00;
 		
 		//Keypad
-		volatile unsigned int num = get_key();
+		unsigned int num = get_key();
 		wait_avr(100); // wait to release button
 		if(num){
-			for (volatile int i = 0; i < num; ++i){
-				SET_BIT(PORTB, 3); // Set LED On
-				wait_avr(100);
-				CLR_BIT(PORTB, 3);
-				wait_avr(100);
-			}
 			//clear
 			clr_lcd();
 			//wait_avr(500);
 			put_lcd(numToChar(num));
 
-		} else
-		{
-			CLR_BIT(PORTB, 3);
-		}
+		} 
 		
 	}
 }
