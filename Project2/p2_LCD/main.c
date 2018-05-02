5a19e5ea73542619b0e982c02dd090d3c8eb830b
Joseph Nguyen <josnguyen.kl@gmail.com>
2018-05-01 20:11:52 -0700

    ready to submiss

--- Project2/p2_LCD/main.c
+++ Project2/p2_LCD/main.c
@@ -16,7 +16,6 @@ char civilH = 12;
 char m = 59;
 char s = 55;
 
-
 /* define default day*/
 int YYYY = 2016;
 int MM = 2;
@@ -30,55 +29,55 @@ unsigned char numToChar(int num)
 {
 	switch (num)
 	{
-		case 1:
+	case 1:
 		return '1';
 		break;
-		case 2:
+	case 2:
 		return '2';
 		break;
-		case 3:
+	case 3:
 		return '3';
 		break;
-		case 4:
+	case 4:
 		return 'A';
 		break;
-		case 5:
+	case 5:
 		return '4';
 		break;
-		case 6:
+	case 6:
 		return '5';
 		break;
-		case 7:
+	case 7:
 		return '6';
 		break;
-		case 8:
+	case 8:
 		return 'B';
 		break;
-		case 9:
+	case 9:
 		return '7';
 		break;
-		case 10:
+	case 10:
 		return '8';
 		break;
-		case 11:
+	case 11:
 		return '9';
 		break;
-		case 12:
+	case 12:
 		return 'C';
 		break;
-		case 13:
+	case 13:
 		return '*';
 		break;
-		case 14:
+	case 14:
 		return '0';
 		break;
-		case 15:
+	case 15:
 		return '#';
 		break;
-		case 16:
+	case 16:
 		return 'D';
 		break;
-		default:
+	default:
 		return 0;
 		break;
 	}
@@ -88,60 +87,59 @@ unsigned char numToInt(int num)
 {
 	switch (num)
 	{
-		case 1:
+	case 1:
 		return 1;
 		break;
-		case 2:
+	case 2:
 		return 2;
 		break;
-		case 3:
+	case 3:
 		return 3;
 		break;
-		case 4:
+	case 4:
 		return 11;
 		break;
-		case 5:
+	case 5:
 		return 4;
 		break;
-		case 6:
+	case 6:
 		return 5;
 		break;
-		case 7:
+	case 7:
 		return 6;
 		break;
-		case 8:
+	case 8:
 		return 11;
 		break;
-		case 9:
+	case 9:
 		return 7;
 		break;
-		case 10:
+	case 10:
 		return 8;
 		break;
-		case 11:
+	case 11:
 		return 9;
 		break;
-		case 12:
+	case 12:
 		return 11;
 		break;
-		case 13:
+	case 13:
 		return 11;
 		break;
-		case 14:
+	case 14:
 		return 0;
 		break;
-		case 15:
+	case 15:
 		return 11;
 		break;
-		case 16:
+	case 16:
 		return 11;
 		break;
-		default:
+	default:
 		return 11;
 		break;
 	}
 }
-
 
 //Need to fix
 unsigned int is_pressed(int r, int c)
@@ -169,7 +167,7 @@ unsigned int get_key()
 		for (c = 0; c < 4; ++c)
 		{
 			if (is_pressed(r, c))
-			return r * 4 + c + 1;
+				return r * 4 + c + 1;
 		}
 	}
 	return 0;
@@ -178,8 +176,8 @@ unsigned int get_key()
 char isLeapYear()
 {
 	return ((YYYY % 4 == 0 && YYYY % 100 != 0) || (YYYY % 400 == 0))
-	? 1 /* true*/
-	: 0 /* false */;
+						 ? 1 /* true*/
+						 : 0 /* false */;
 }
 
 void increaseDay()
@@ -210,7 +208,8 @@ void increaseDay()
 	}
 }
 
-void militaryToCivilTime() {
+void militaryToCivilTime()
+{
 	if (h == 0)
 	{
 		civilH = 12;
@@ -254,22 +253,21 @@ void increaseTime()
 	militaryToCivilTime();
 }
 
-
 int setDay(int day, int month, int year)
 {
 	if (month > 12 || month == 0 || year == 0 || day == 0)
-	return 0; //Error
+		return 0; //Error
 
 	if (day > days_of_month[month])
 	{
 		if (month != 2)
-		return 0; //need to check for leap year
+			return 0; //need to check for leap year
 		else
 		{
 			if (isLeapYear() && day > 29)
-			return 0;
+				return 0;
 			if (!isLeapYear())
-			return 0;
+				return 0;
 		}
 	}
 	DD = day;
@@ -281,7 +279,7 @@ int setDay(int day, int month, int year)
 int setTime(char hour, char minute, char second)
 {
 	if (hour > 23 || minute > 59 || second > 59)
-	return 0;
+		return 0;
 
 	h = hour;
 	m = minute;
@@ -289,33 +287,37 @@ int setTime(char hour, char minute, char second)
 	return 1;
 }
 
-
-void printMsg(const char * str){
+void printMsg(const char *str)
+{
 	clr_lcd();
 	sprintf(bufmsg, str);
 	pos_lcd(0, 0);
 	puts_lcd2(bufmsg);
 }
 
-unsigned int confirm(){
+unsigned int confirm()
+{
 	int key = 0;
 	int pressed = 0;
 	int check = 0;
-	
-	while(!pressed){
+
+	while (!pressed)
+	{
 		key = get_key();
-		
-		if(key == 12)
+
+		if (key == 12)
 		{
 			check = 0;
 			pressed = 1;
-			} else if (key == 16){
+		}
+		else if (key == 16)
+		{
 			pressed = 1;
 			check = 1;
 		}
 		wait_avr(100);
 	}
-	
+
 	return check;
 }
 
@@ -331,8 +333,8 @@ void getInputAndSetDay()
 	int n = 11;
 	int n2;
 	int dd, mm, yyyy;
-	
-	while (current < 10 )
+
+	while (current < 10)
 	{
 		/*Print the dash MM/DD/YYYY*/
 		if (current == 2 || current == 5)
@@ -343,7 +345,7 @@ void getInputAndSetDay()
 		}
 
 		n = get_key();
-		
+
 		wait_avr(250);
 		n2 = numToInt(n);
 
@@ -355,21 +357,25 @@ void getInputAndSetDay()
 			++current;
 		}
 	}
-	
-	if (confirm() == 0) {
+
+	if (confirm() == 0)
+	{
 		printMsg("CANCELED");
-		} else {
+	}
+	else
+	{
 		/*MM/DD/YYYY*/
 		mm = in[0] * 10 + in[1];
 		dd = in[3] * 10 + in[4];
 		yyyy = in[6] * 1000 + in[7] * 100 + in[8] * 10 + in[9];
 
-		
 		char c = setDay(dd, mm, yyyy);
 		if (!c)
 		{
 			printMsg("Invalid Input");
-			} else {
+		}
+		else
+		{
 			printMsg("Date is set!");
 		}
 	}
@@ -412,9 +418,12 @@ void getInputAndSetTime()
 		}
 	}
 
-	if (confirm() == 0){
+	if (confirm() == 0)
+	{
 		printMsg("CANCELED");
-		} else {
+	}
+	else
+	{
 		/*hh:mm:ss*/
 		char hh, mm, ss;
 		hh = in[0] * 10 + in[1];
@@ -425,11 +434,13 @@ void getInputAndSetTime()
 		if (!c)
 		{
 			printMsg("Invalid Input");
-			} else {
+		}
+		else
+		{
 			printMsg("Time is set!");
 		}
 	}
-	
+
 	wait_avr(2000);
 }
 
@@ -446,19 +457,16 @@ void displayInfo()
 	if (isCivil)
 	{
 		if (AM)
-		sprintf(bufTime, "%02i:%02i:%02i AM", civilH, m, s);
+			sprintf(bufTime, "%02i:%02i:%02i AM", civilH, m, s);
 		else
-		sprintf(bufTime, "%02i:%02i:%02i PM", civilH, m, s);
-		
+			sprintf(bufTime, "%02i:%02i:%02i PM", civilH, m, s);
 	}
 	else
-	sprintf(bufTime, "%02i:%02i:%02i", h, m, s);
-	
+		sprintf(bufTime, "%02i:%02i:%02i", h, m, s);
+
 	pos_lcd(1, 0);
 	puts_lcd2(bufTime);
 }
-
-
 
 int main(void)
 {
@@ -495,9 +503,9 @@ int main(void)
 				isCivil = 0;
 			else
 				isCivil = 1;
+			displayInfo();
 		}
 		wait_avr(949);
 		increaseTime();
-
 	}
 }
