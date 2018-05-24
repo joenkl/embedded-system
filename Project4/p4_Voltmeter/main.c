3f1e94bd538c0a82b145b2d94d790bfa52f6abd4
Kha Bui <khadbui@gmail.com>
2018-05-24 15:38:18 -0700

    Update Project 4 - Completed

--- Project4/p4_Voltmeter/main.c
+++ Project4/p4_Voltmeter/main.c
@@ -1,66 +1,94 @@
 /*
-* p4_Voltmeter.c
+* p4_VoltMeter.c
 *
+* Created: 5/24/2018 3:13:46 PM
+* Author : Kha Bui, Joseph Luan Nguyen
 */
 
 #include <avr/io.h>
+#include "stdio.h"
 #include "avr.h"
-#include "lcd.h"
 #include "keypad.h"
-#include "voltmeter.h"
+#include "lcd.h"
+
+char bufMsg[17];
+
+void ini_meter(){
+	sprintf(bufMsg, "Ins:____Avg:____");
+	pos_lcd(0,0);
+	puts_lcd2(bufMsg);
+	
+	sprintf(bufMsg, "Min:____Max:____");
+	pos_lcd(1,0);
+	puts_lcd2(bufMsg);
+}
+
+unsigned short get_A2D(){
+	ADMUX |= (1<<REFS0);
+	SET_BIT(ADCSRA, 7); //Set ADEN
+	SET_BIT(ADCSRA, 6); //Set ADSC start conversion
+	while(GET_BIT(ADCSRA, 6)); //wait to finish measure
+	
+	return ADC;
+}
 
 int main(void)
 {
-	//Set all port A to input
-	DDRA = 0x00;
-	
 	ini_avr();
 	ini_lcd();
 	ini_meter();
-	unsigned short volt = 0;
+	float curr = 0.0;
+	float avg = 0.0;
+	float min = 0.0;
+	float max = 0.0;
+	float total = 0.0;
+	unsigned short count = 0;
+
 	while (1)
 	{
-		//start meter Key A
 		if(get_key() == 4){
 			while(1){
-				//Reset meter B
 				if(get_key() == 8){
 					ini_meter();
+					curr = 0.0;
+					avg = 0.0;
+					min = 0.0;
+					max = 0.0;
+					total = 0.0;
+					count = 0;
 					break;
 				}
 				
+				curr = (float)get_A2D() / 1023.0 * 5.0;
 				++count;
-				//A2D is 1023 max at 5Volt
-				volt = ((float)get_A2D()/ 1023.0 * 5.0) * 100;
 				
-				//1st time reading data
 				if(count == 1){
-					minVol = volt;
-					maxVol = volt;
+					min = curr;
+					max = curr;
+				}
+
+				if (curr < min){
+					min = curr;
 				}
 				
-				currVol = volt;
-				//Calculate min
-				if (currVol < minVol){
-					minVol = currVol;
+				if(curr > max){
+					max = curr;
 				}
 				
-				//Calculate max
-				if(currVol > maxVol){
-					maxVol = currVol;
-				}
+				total += curr;
+				avg = total / (float)count;
 				
-				total += volt;
-				avgVol = total/count;
+				sprintf(bufMsg, "Ins:%.2fAvg:%.2f", curr, avg);
+				pos_lcd(0,0);
+				puts_lcd2(bufMsg);
 				
-				displayAvgVol();
-				displayCurrVol();
-				displayMaxVol();
-				displayMinVol();
+				sprintf(bufMsg, "Min:%.2fMax:%.2f", min, max);
+				pos_lcd(1,0);
+				puts_lcd2(bufMsg);
 				
-				//wait 500ms
 				wait_avr(500);
 			}
 		}
 	}
 }
+
