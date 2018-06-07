c6a895c86309f1f272988843cb581ce7c9f6792f
Joseph Nguyen <josnguyen.kl@gmail.com>
2018-06-07 14:51:35 -0700

    change to wait 1200 on turn

--- p5/p5/motor.c
+++ p5/p5/motor.c
@@ -11,12 +11,14 @@
 //Port C6 - forward
 #include "motor.h"
 
-void ini_motor(){
+void ini_motor()
+{
 	DDRC = 0xF0;
 	PORTC = 0x00;
 }
 
-void goForward(){
+void goForward()
+{
 	//left
 	SET_BIT(PORTC, 4);
 	CLR_BIT(PORTC, 5);
@@ -25,15 +27,17 @@ void goForward(){
 	CLR_BIT(PORTC, 7);
 }
 
-void goBackward(){
+void goBackward()
+{
 	SET_BIT(PORTC, 5);
 	CLR_BIT(PORTC, 4);
-	
+
 	SET_BIT(PORTC, 7);
 	CLR_BIT(PORTC, 6);
 }
 
-void stopMotor(){
+void stopMotor()
+{
 	//right
 	CLR_BIT(PORTC, 4);
 	CLR_BIT(PORTC, 5);
@@ -42,34 +46,37 @@ void stopMotor(){
 	CLR_BIT(PORTC, 7);
 }
 
-void turnLeft(){
+void turnLeft()
+{
 	//stop left motor
 	CLR_BIT(PORTC, 4);
 	CLR_BIT(PORTC, 5);
 	//go forward right motor
 	SET_BIT(PORTC, 6);
 	CLR_BIT(PORTC, 7);
-	
+
 	wait_avr(1500);
 	stopMotor();
 }
 
-void turnRight(){
+void turnRight()
+{
 	//stop right motor
 	CLR_BIT(PORTC, 6);
 	CLR_BIT(PORTC, 7);
 	//go forward left motor
 	SET_BIT(PORTC, 4);
 	CLR_BIT(PORTC, 5);
-	
-	wait_avr(1500);
+
+	wait_avr(1200);
 	stopMotor();
 }
 
 //connect to B3
-void motorSpeed(unsigned int speed){
-		SET_BIT(DDRB, 3);
-		TCCR0 = 0x75;
-		TIMSK = 0x00;
-		OCR0 = (255 - speed);
+void motorSpeed(unsigned int speed)
+{
+	SET_BIT(DDRB, 3);
+	TCCR0 = 0x75;
+	TIMSK = 0x00;
+	OCR0 = (255 - speed);
 }
